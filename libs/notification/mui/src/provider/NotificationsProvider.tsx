5cd6b2acd00235d13c66f19e4329ca99e2ee0649
dmuharemagic <dmuharemagic@croz.net>
2022-06-14 09:04:24 +0200

    Add NotificationsProvider wrapper component to MUI notification module, export into main index file

---
+++ libs/notification/mui/src/provider/NotificationsProvider.tsx
@@ -0,0 +1,71 @@
+import React, { ReactNode } from "react";
+import {
+  Alert, AlertColor, AlertTitle, Snackbar,
+} from "@mui/material";
+import { useNotifications } from "@nrich/notification-core";
+
+export type NotificationPosition = | "top-left"
+| "top-right"
+| "top-center"
+| "bottom-left"
+| "bottom-right"
+| "bottom-center";
+
+export type NotificationOrigin = ["top" | "bottom", "left" | "right" | "center"];
+
+export interface NotificationsProviderProps {
+  autoClose?: number;
+  position?: NotificationPosition;
+  children: ReactNode
+}
+
+/**
+ * A provider component used to wrap the container in which the notifications are displayed.
+ *
+ * @param position specifies the notification position on the page,
+ * derived from a set of predefined positions available in MUI
+ * @param autoClose the duration after which the notification closes itself
+ * (if left undefined, it doesn't close)
+ * @param children the children components from the wrapped component
+ */
+const NotificationsProvider = ({ position = "bottom-right", autoClose, children }: NotificationsProviderProps) => {
+  const positioning = position.split("-") as NotificationOrigin;
+
+  const [notifications, remove] = useNotifications();
+
+  return (
+    <>
+      {
+        notifications.map((notification) => (
+          <Snackbar
+            key={notification.date}
+            open
+            autoHideDuration={autoClose}
+            onClose={() => remove(notification)}
+            anchorOrigin={{
+              vertical: positioning[0],
+              horizontal: positioning[1],
+            }}
+          >
+
+            <Alert
+              onClose={() => remove(notification)}
+              severity={notification.severity.toLowerCase() as AlertColor}
+            >
+              <AlertTitle><strong>{notification.title}</strong></AlertTitle>
+              {notification.content}
+              {notification.messageList?.length > 0 && (
+                <ul>
+                  {notification.messageList.map((message) => <li>{message}</li>)}
+                </ul>
+              )}
+            </Alert>
+          </Snackbar>
+        ))
+      }
+      {children}
+    </>
+  );
+};
+
+export default NotificationsProvider;
---
+++ libs/notification/mui/src/provider/index.ts
@@ -0,0 +1,1 @@
+export { default as NotificationsProvider } from "./NotificationsProvider";
