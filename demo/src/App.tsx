da932dfd3229fd5eaf8c9602f134f702758ccc29
dmuharemagic <dmuharemagic@croz.net>
2022-06-14 09:04:57 +0200

    Showcase core and MUI notification modules in demo app

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -7,8 +7,19 @@ import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { Link, Route, Routes } from "react-router-dom";
+import Notifications from "./pages/Notifications";
 
 const drawerWidth = 240;
+const routes = [
+  {
+    path: "notifications",
+    title: "Notifications",
+  },
+];
 
 const App = () => (
   <Box sx={{ display: "flex" }}>
@@ -30,12 +41,23 @@ const App = () => (
     >
       <Toolbar />
       <Box sx={{ overflow: "auto" }}>
-        <List />
+        <List>
+          {routes.map((menuItem) => (
+            <ListItem key={menuItem.path} disablePadding>
+              <ListItemButton component={Link} to={menuItem.path}>
+                <ListItemText primary={menuItem.title} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
         <Divider />
       </Box>
     </Drawer>
     <Box component="main" sx={{ flexGrow: 1, m: 2 }}>
       <Toolbar />
+      <Routes>
+        <Route path="notifications" element={<Notifications />} />
+      </Routes>
     </Box>
   </Box>
 );
--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { fetchInterceptor, xhrInterceptor } from "@nrich/notification-core";
+import { NotificationsProvider } from "@nrich/notification-mui";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+
+fetchInterceptor();
+xhrInterceptor();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <NotificationsProvider>
+        <App />
+      </NotificationsProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
