2e87b54c27e4ce2a9f7d05f3a1bffea6a920c736
agrancaric <agrancaric@croz.net>
2022-06-15 08:41:19 +0200

    Add comments for notification types properties

--- libs/notification/core/src/api/notification/types.ts
+++ libs/notification/core/src/api/notification/types.ts
@@ -1,14 +1,45 @@
 export type NotificationSeverity = "INFO" | "WARNING" | "ERROR";
 
 export interface NotificationResponse {
+
+  /**
+   * Title of the notification.
+   */
   title: string;
+
+  /**
+   * Content of the notification (i.e. for exception
+   * notification this will contain resolved message for exception or 'Error occurred' text).
+   */
   content: string;
+
+  /**
+   * List of messages (i.e. for validation failure notification
+   * this will contain all validation failure messages).
+   */
   messageList: string[];
+
+  /**
+   * Severity indicating importance of notification.
+   */
   severity: NotificationSeverity;
+
+  /**
+   * Custom override data sent from the server. Each component library will have its own data here.
+   * For example current supported options in mui implementation are position and autoClose.
+   */
   uxNotificationOptions?: Record<string, unknown>;
 }
 
 export interface NotificationDataResponse<T> {
+
+  /**
+   * Response data
+   */
   data: T,
+
+  /**
+   * Notification
+   */
   notification: NotificationResponse
 }
