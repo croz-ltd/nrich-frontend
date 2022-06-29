f6708e8ce616eb2132c23ee5a5a362abb58d11c7
agrancaric <agrancaric@croz.net>
2022-06-29 16:33:48 +0200

    Implement conversion of FormConfiguration to Yup schema

---
+++ libs/form-configuration/core/src/converter/FormConfigurationValidationConverter.ts
@@ -0,0 +1,101 @@
+import * as yup from "yup";
+
+import { ConstrainedPropertyClientValidatorConfiguration, ConstrainedPropertyConfiguration, ValidatorConverter } from "../api";
+
+export class FormConfigurationValidationConverter {
+  private static PATH_SEPARATOR = ".";
+
+  private static DEFAULT_CONVERTER_LIST: ValidatorConverter[] = [
+    {
+      supports: (configuration) => ["NotNull", "NotBlank", "NotEmpty"].includes(configuration.name),
+      convert: (configuration, validator) => validator.required(configuration.errorMessage),
+    },
+    {
+      supports: (configuration) => ["Size", "Length"].includes(configuration.name),
+      convert: (configuration, validator) => validator.min(configuration.argumentMap.min, configuration.errorMessage)
+        .max(configuration.argumentMap.max, configuration.errorMessage),
+    },
+    {
+      supports: (configuration) => ["Pattern"].includes(configuration.name),
+      convert: (configuration, validator) => validator.matches(configuration.argumentMap.pattern, configuration.errorMessage),
+    },
+    {
+      supports: (configuration) => ["Min", "Max"].includes(configuration.name),
+      convert: (configuration, validator) => validator[configuration.name.toLowerCase()](configuration.argumentMap.value, configuration.errorMessage),
+    },
+    {
+      supports: () => true,
+      convert: (configuration, validator) => validator[configuration.name.toLowerCase()](configuration.errorMessage),
+    },
+  ];
+
+  private readonly additionalConverters: ValidatorConverter[];
+
+  constructor(additionalConverters: ValidatorConverter[] = []) {
+    this.additionalConverters = additionalConverters;
+  }
+
+  convertFormConfigurationToYupSchema(constrainedPropertyConfigurationList: ConstrainedPropertyConfiguration[]): yup.ObjectSchema<any> {
+    return this.convertFormConfigurationToYupSchemaInternal(constrainedPropertyConfigurationList);
+  }
+
+  private convertFormConfigurationToYupSchemaInternal(constrainedPropertyConfigurationList: ConstrainedPropertyConfiguration[]) {
+    let schema = yup.object().shape({});
+
+    constrainedPropertyConfigurationList.forEach((property) => {
+      const yupValidation = yup[property.javascriptType];
+
+      if (!yupValidation) {
+        return;
+      }
+
+      const validator = property.validatorList.reduce((previousValidator, validatorConfiguration) => this.applyConverter(validatorConfiguration, previousValidator), yupValidation());
+
+      const [propertyName, restOfPathList] = FormConfigurationValidationConverter.convertPath(property.path);
+      const currentPropertySchema = yup.object().shape({ [propertyName]: validator });
+
+      if (restOfPathList.length > 0) {
+        const currentPathSchema = [...restOfPathList].reverse()
+          .reduce((currentShape, path) => ({ [path]: yup.object().shape(currentShape) }), { [propertyName]: validator });
+
+        schema = schema.concat(yup.object().shape(currentPathSchema));
+      }
+      else {
+        schema = schema.concat(currentPropertySchema);
+      }
+    });
+
+    return schema;
+  }
+
+  private applyConverter(validatorConfiguration: ConstrainedPropertyClientValidatorConfiguration, validator: any): any {
+    const converter = this.resolveConverter(validatorConfiguration);
+    let resolvedValidator = validator;
+
+    if (converter) {
+      try {
+        resolvedValidator = converter.convert(validatorConfiguration, validator);
+      }
+      catch (ignore) {
+        // constraint is not registered so skip evaluation
+      }
+    }
+
+    return resolvedValidator;
+  }
+
+  private resolveConverter(validatorConfiguration: ConstrainedPropertyClientValidatorConfiguration) {
+    const allConverters = this.additionalConverters.concat(FormConfigurationValidationConverter.DEFAULT_CONVERTER_LIST);
+
+    return allConverters.find((additionalConverter) => additionalConverter.supports(validatorConfiguration));
+  }
+
+  private static convertPath(path: string): [string, string[]] {
+    const pathList = path.split(this.PATH_SEPARATOR);
+    const propertyName = pathList[pathList.length - 1];
+
+    pathList.pop();
+
+    return [propertyName, pathList];
+  }
+}
---
+++ libs/form-configuration/core/test/testutil/form-configuration-generating-util.ts
@@ -0,0 +1,129 @@
+export const invalidValidationConfiguration = () => [
+  {
+    path: "type",
+    propertyType: "net.croz.Type",
+    javascriptType: "unknown",
+    validatorList: [
+      {
+        name: "NotEmpty",
+        argumentMap: {},
+        errorMessage: "Type cannot be empty",
+      },
+    ],
+  },
+  {
+    path: "number",
+    propertyType: "java.lang.Integer",
+    javascriptType: "number",
+    validatorList: [
+      {
+        name: "Pattern",
+        argumentMap: { pattern: "[0-9]+" },
+        errorMessage: "Number must contain only digits",
+      },
+    ],
+  },
+];
+
+export const createSimpleValidationList = () => [
+  {
+    path: "username",
+    propertyType: "java.lang.String",
+    javascriptType: "string",
+    validatorList: [
+      {
+        name: "NotBlank",
+        argumentMap: {},
+        errorMessage: "Username cannot be blank",
+      },
+    ],
+  },
+];
+
+export const createComplexValidationList = () => [
+  {
+    path: "name",
+    propertyType: "java.lang.String",
+    javascriptType: "string",
+    validatorList: [
+      {
+        name: "Pattern",
+        argumentMap: { pattern: "[a-zA-Z]+" },
+        errorMessage: "Name must contain only letters",
+      },
+      {
+        name: "Size",
+        argumentMap: { min: 3, max: 10 },
+        errorMessage: "Name must have minimum 3 and maximum 10 characters",
+      },
+    ],
+  },
+  {
+    path: "age",
+    propertyType: "java.lang.Integer",
+    javascriptType: "number",
+    validatorList: [
+      {
+        name: "Min",
+        argumentMap: { value: 21 },
+        errorMessage: "Minimum age is 21",
+      },
+      {
+        name: "Max",
+        argumentMap: { value: 110 },
+        errorMessage: "Maximum age is 110",
+      },
+    ],
+  },
+];
+
+export const createNestedValidationList = () => [
+  {
+    path: "user.username",
+    propertyType: "java.lang.String",
+    javascriptType: "string",
+    validatorList: [
+      {
+        name: "NotBlank",
+        argumentMap: {},
+        errorMessage: "Username cannot be blank",
+      },
+    ],
+  },
+  {
+    path: "user.address.street",
+    propertyType: "java.lang.String",
+    javascriptType: "string",
+    validatorList: [
+      {
+        name: "NotBlank",
+        argumentMap: {},
+        errorMessage: "Street cannot be blank",
+      },
+    ],
+  },
+  {
+    path: "user.address.city",
+    propertyType: "java.lang.String",
+    javascriptType: "string",
+    validatorList: [
+      {
+        name: "NotBlank",
+        argumentMap: {},
+        errorMessage: "City cannot be blank",
+      },
+    ],
+  },
+  {
+    path: "user.email",
+    propertyType: "java.lang.String",
+    javascriptType: "string",
+    validatorList: [
+      {
+        name: "Email",
+        argumentMap: {},
+        errorMessage: "Not a valid email",
+      },
+    ],
+  },
+];
