784706ff1f46944c72beb02762e283e17ae4f074
edivic <edivic@croz.net>
2022-07-07 15:40:48 +0200

    Expanded fetch-form-configurations test with additional validator converters

--- libs/form-configuration/core/test/loader/fetch-form-configurations.test.ts
+++ libs/form-configuration/core/test/loader/fetch-form-configurations.test.ts
@@ -1,6 +1,7 @@
 import { fetchFormConfigurations } from "../../src";
 import { useFormConfigurationStore } from "../../src/store/form-configuration-store";
-import { mockFormConfigurations } from "../testutil/form-configuration-generating-util";
+import { useValidatorConverterStore } from "../../src/store/validator-converter-store";
+import { mockFormConfigurations, mockValidatorConverters } from "../testutil/form-configuration-generating-util";
 import { setupFormConfigurationServer } from "../testutil/setup-form-configuration-server";
 
 const server = setupFormConfigurationServer();
@@ -15,7 +16,7 @@ afterAll(() => server.close());
 describe("@nrich/form-configuration-core/fetch-form-configurations", () => {
   it("should resolve form configurations", async () => {
     // when
-    const response = await fetchFormConfigurations({ url: "/test-url" });
+    const response = await fetchFormConfigurations({ url: "/test-url", additionalValidatorConverters: mockValidatorConverters });
 
     // then
     expect(response).toBeDefined();
@@ -23,11 +24,16 @@ describe("@nrich/form-configuration-core/fetch-form-configurations", () => {
 
     // and when
     const formConfigurationState = useFormConfigurationStore.getState();
+    const validationConverterState = useValidatorConverterStore.getState();
 
     // then
     expect(formConfigurationState.formConfigurations).toHaveLength(2);
     expect(formConfigurationState.formConfigurations[0]).toMatchObject(mockFormConfigurations[0]);
     expect(formConfigurationState.formConfigurations[1]).toMatchObject(mockFormConfigurations[1]);
+
+    expect(validationConverterState.validatorConverters).toHaveLength(2);
+    expect(validationConverterState.validatorConverters[0]).toMatchObject(mockValidatorConverters[0]);
+    expect(validationConverterState.validatorConverters[1]).toMatchObject(mockValidatorConverters[1]);
 
     // cleanup
     formConfigurationState.set([]);
